5b7e21057943099492eaa457e650af223b5d3543
Alexander <name.way@gmail.com>
2024-05-06 23:39:41 +0300

    initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,35 @@
+import MiniCssExtractPlugin, { loader } from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:5]",
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, scssLoader];
+}
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from '../../components/Counter'
+
+const MainPage = () => {
+  return (
+    <>
+    <div>MainPage</div>
+    <Counter />
+    </>
+
+  )
+}
+
+export default MainPage
