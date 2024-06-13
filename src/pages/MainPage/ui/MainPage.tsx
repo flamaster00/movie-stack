c22027172d4522822429b014a56d3fff8b1e76ee
Alexander <name.way@gmail.com>
2024-06-13 20:37:04 +0300

    remove Counter from MainPage.tsx

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
   const { t } = useTranslation('mainpage');
   return (
     <div>
       {t('MainPage')}
-      <Counter />
     </div>
   );
 };
